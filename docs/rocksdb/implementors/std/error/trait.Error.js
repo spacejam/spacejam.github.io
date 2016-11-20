f96d875f0a9aa2e958936bfc68ac96f8b504dbca
Tyler Neely <t@jujit.su>
2016-11-20 14:08:41 +0100

    update docs

--- docs/rocksdb/implementors/std/error/trait.Error.js
+++ docs/rocksdb/implementors/std/error/trait.Error.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["libc"] = [];
+implementors["libc"] = [];implementors["rocksdb"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='struct' href='rocksdb/struct.Error.html' title='rocksdb::Error'>Error</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
